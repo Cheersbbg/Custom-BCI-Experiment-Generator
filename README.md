96205cb7d9135579453d4b3a1c21ee8c8bead9fd
cheersbbg <83891351+Cheersbbg@users.noreply.github.com>
2023-12-27 04:23:24 -0800

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Custom-BCI-Experiment-Generator
+This repo contains two components that creates completely customizable and out-of-the-box experiment paradigms for conducting body scanning: 
+
+Hands Template Generation and Body Annotation Interface
+
+More specific manual and a docker will be updated soon:)
---
+++ hands/hands-generator.py
@@ -0,0 +1,84 @@
+import os
+import pickle
+import cv2
+import numpy as np
+from PIL import Image, ImageChops
+import argparse
+
+def custom_experiment(list_of_lists, reset, exp_name, is_white=True):
+    """
+    Creates experiment and map images based on given contours.
+
+    Args:
+    list_of_lists: List of lists containing contour indices.
+    reset: Boolean to control background reset for each image.
+    exp_name: Name of the new experiment.
+    is_white: Boolean to choose the background color.
+
+    Returns:
+    Tuple containing paths to the experiment and map directories.
+    """
+    contours = np.load('handcountours.npy', allow_pickle=True)
+    template = cv2.imread('blackhand.png' if is_white else 'whitehand.png')
+    inv_img = ImageChops.invert(Image.fromarray(template)).convert("RGBA")
+
+    exp_dir = os.path.join(os.getcwd(), exp_name)
+    exp_path = os.path.join(exp_dir, "exp")
+    map_path = os.path.join(exp_dir, "map")
+
+    os.makedirs(exp_path, exist_ok=True)
+    os.makedirs(map_path, exist_ok=True)
+
+    for key, contour_list in enumerate(list_of_lists):
+        background = np.zeros((600, 800)) if not reset else background.copy()
+        for contour in contour_list:
+            cv2.drawContours(background, [contours[contour]], 0, color=(255, 255, 255), thickness=-1)
+
+        map_image_path = os.path.join(map_path, f'image{key}.png')
+        exp_image_path = os.path.join(exp_path, f'image{key}.png')
+
+        Image.fromarray(background).convert("L").save(map_image_path)
+        blend_and_save_images(map_image_path, inv_img, exp_image_path)
+
+    return exp_path, map_path
+
+def blend_and_save_images(map_image_path, inv_img, exp_image_path):
+    """
+    Blends the map image with the inverted template and saves the result.
+
+    Args:
+    map_image_path: Path to the map image.
+    inv_img: Inverted image of the template.
+    exp_image_path: Path to save the blended experiment image.
+    """
+    temp = cv2.imread(map_image_path)
+    b, g, r = cv2.split(temp)
+    after = cv2.merge([b * 2, g * 0, r * 2])
+    after = ImageChops.invert(Image.fromarray(after)).convert("RGBA")
+    alpha_blended = Image.blend(after, inv_img, alpha=0.6)
+    alpha_blended.save(exp_image_path)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Custom BCI Experiment Generator")
+    parser.add_argument("--exp_name", type=str, help="Name of the new experiment",default="SingleFinger")
+    parser.add_argument("--list_of_lists", type=str, help="List of lists containing contour indices", default="[[35, 25, 11], [19, 31, 21], [15, 17, 13], [27, 29, 23], [37, 39, 33], [36, 38, 32], [26, 28, 22], [14, 16, 12], [18, 30, 20], [34, 24, 10]]")
+    args = parser.parse_args()
+    EXP_NAME = args.exp_name
+    LIST_OF_LISTS = eval(args.list_of_lists)
+
+    # Load history of experiments
+    if os.path.exists('history_dict.pickle'):
+        with open('history_dict.pickle', 'rb') as f:
+            history_exps = pickle.load(f)
+    else:
+        history_exps = {}
+
+    if EXP_NAME not in history_exps.keys():
+        exp_path, map_exp = custom_experiment(LIST_OF_LISTS, reset=False, exp_name=EXP_NAME, is_white=True)
+        history_exps[EXP_NAME] = exp_path
+
+        with open('history_dict.pickle', 'wb') as f:
+            pickle.dump(history_exps, f)
+    else:        exp_path = history_exps[EXP_NAME]
+
